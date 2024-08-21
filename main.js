af16e6dadfe2636cc4de847943ca8fc4a4659a11
Denis Gorbunov <volo_non_valeo@mail.ru>
2024-08-21 15:51:39 +0300

    Внесены правки по результатам кодревью

---
+++ main.js
@@ -0,0 +1,24 @@
+const headers = Array.from(document.querySelectorAll(".accordion__header"));
+const burger = document.querySelector(".header__burger");
+console.log(burger);
+
+headers.forEach((header) => {
+    header.addEventListener("click", accordionHandler);
+});
+
+function accordionHandler(e) {
+    let currentHeader = e.target.closest(".accordion__item");
+    let currentCaption = currentHeader.querySelector(".accordion__caption");
+    currentHeader.classList.toggle("is-active");
+    if (currentHeader.classList.contains("is-active")) {
+        currentCaption.textContent = "свернуть"
+    } else {
+        currentCaption.textContent = "подробнее"
+    };
+}
+
+burger.addEventListener("click", burgerHandler);
+
+function burgerHandler() {
+    burger.classList.toggle("active");
+}
